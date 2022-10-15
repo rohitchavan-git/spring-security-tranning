5c6b5a5d5e10cf87bcb90e69df42cbc766d906a5
rohit <rohitchavan8058@gmail.com>
2022-10-15 21:49:33 +0530

    Initial commit

---
+++ src/main/java/com/rohit/springsecuritytranning/SpringSecurityTranningApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.springsecuritytranning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTranningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTranningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rohit/springsecuritytranning/SpringSecurityTranningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.springsecuritytranning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTranningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
